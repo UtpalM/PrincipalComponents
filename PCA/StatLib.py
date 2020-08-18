9570c9c4bcf805d413dbd18761977c2baac5af9f
Utpal Mattoo <30846264+UtpalM@users.noreply.github.com>
2020-08-18 09:31:34 -0400

    Update StatLib.py

--- PCA/StatLib.py
+++ PCA/StatLib.py
@@ -6,8 +6,8 @@ Source:
 http://python-for-multivariate-analysis.readthedocs.io/a_little_book_of_
        python_for_multivariate_analysis.html
        
-This source code from the above location was taken, slightly modified 
-and modularized/structed for better clarity
+This source code from the above location was modified 
+and structured for better readability
 """
 import numpy as np
 
